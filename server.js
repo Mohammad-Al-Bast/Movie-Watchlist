af1bf1258c3db36506f12d4383988de44ae61566
Mohammad Al Bast <mohammadbast4@gmail.com>
2025-12-20 01:29:39 +0200

    Set up Express server with routes for movies and categories

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 import express from "express";
+import moviesRouter from "./routes/movies. js";
+import categoriesRouter from "./routes/categories.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// New movie watchlist routes
+app.use("/api/v1/movies", moviesRouter);
+app.use("/api/v1/categories", categoriesRouter);
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
